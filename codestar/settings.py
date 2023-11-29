8255944b1cd062b1592db95e31281350b7ec390d
Ry-F3 <rhysfeew@gmail.com>
2023-11-29 12:02:59 +0000

    Install packages

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://bogger-on-the-move-django-ce2a77c17409.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['django-blogger.herokuapp.com', 'localhost']
 
 
 # Application definition

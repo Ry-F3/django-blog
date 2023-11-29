5b88aec0037784441276291106034a751e846106
Ry-F3 <rhysfeew@gmail.com>
2023-11-29 12:06:42 +0000

    update allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-blogger.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['django-blogger-7390ecfe1b6f.herokuapp.com', 'localhost']
 
 
 # Application definition

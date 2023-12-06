46743b33ddd3c631c57df0cb9d3c270dae687711
Ry-F3 <rhysfeew@gmail.com>
2023-12-06 21:31:02 +0000

    Add heroku url

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,11 +30,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-ryf3-djangoblog-bopx0wj4oov.ws-eu106.gitpod.io']
+ALLOWED_HOSTS = ['django-blogger-7390ecfe1b6f.herokuapp.com', 'localhost']
 
 # 'django-blogger-7390ecfe1b6f.herokuapp.com', 'localhost'
+# '8000-ryf3-djangoblog-bopx0wj4oov.ws-eu106.gitpod.io'
 
 
 # Application definition
